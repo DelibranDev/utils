b6787dc8e6bea63af1b94d550364d3e7daecb678
Antonio Ruiz <contacto.antonioruiz@gmail.com>
2026-01-11 17:27:40 +0100

    update shoppingCart

--- src/components/order/orderShoppingCart.jsx
+++ src/components/order/orderShoppingCart.jsx
@@ -1,25 +1,92 @@
 import React from "react";
 
-export const OrderShoppingCart = ({ data, CartActions }) => {
+export const OrderShoppingCart = ({ data, CartActions, size = "normal" }) => {
   const Product = ({ data }) => {
     return (
       <>
-        <div className="product-cart">
+        <div
+          className="product-cart"
+          style={
+            size !== "normal"
+              ? {
+                  borderRadius: "0",
+                  borderLeft: "0",
+                  borderRight: "0",
+                  borderBottom: "0",
+                }
+              : {}
+          }
+        >
+          {size === "normal" && (
+            <div
+              className="product-image background-image"
+              style={{
+                backgroundImage:
+                  data &&
+                  data.Product &&
+                  data.Product.images.length > 0 &&
+                  data?.Product.images?.[0]
+                    ? `url('${data.Product.images[0]}')`
+                    : "no",
+              }}
+            ></div>
+          )}
           <div
-            className="product-image background-image"
-            style={{
-              backgroundImage:
-                data && data.Product && data.Product.images.length > 0 && data?.Product.images?.[0] ? `url('${data.Product.images[0]}')` : "no",
-            }}
-          ></div>
-          <div className="product-cart-info">
-            <div className="product-details">
-              <div className="product-name">{data && data.Product && data.Product.name ? data.Product.name : ""}</div>
-              <div className="product-description">{data?.Product?.description || ""}</div>
+            className="product-cart-info"
+            style={
+              size === "normal"
+                ? { flexDirection: "column" }
+                : { flexDirection: "row" }
+            }
+          >
+            <div
+              className="product-details"
+              style={
+                size !== "normal"
+                  ? {
+                      borderRadius: "0",
+                      borderLeft: "0",
+                      borderRight: "0",
+                      borderBottom: "0",
+                      marginTop: "4px",
+                    }
+                  : {}
+              }
+            >
+              <div
+                className="product-name"
+                style={
+                  size !== "normal"
+                    ? {
+                        fontWeight: "500",
+                      }
+                    : {}
+                }
+              >
+                {data && data.Product && data.Product.name
+                  ? data.Product.name
+                  : ""}
+              </div>
+              {size === "normal" && (
+                <div className="product-description">
+                  {data?.Product?.description || ""}
+                </div>
+              )}
             </div>
             <div className="product-details-bottom">
-              <div className="product-name">{data?.Product?.price ? data.Product.price.toFixed(2) : "0.00"}€</div>
-              <div className="product-actions">
+              <div className="product-name">
+                {data?.Product?.price ? data.Product.price.toFixed(2) : "0.00"}€
+              </div>
+              <div
+                className="product-actions"
+                style={
+                  size !== "normal"
+                    ? {
+                        marginLeft: "5px",
+                      }
+                    : {}
+                }
+              >
                 <CartActions data={data} />
               </div>
             </div>
@@ -31,7 +98,9 @@ export const OrderShoppingCart = ({ data, CartActions }) => {
 
   return (
     <div className="order-products">
-      {Array.isArray(data?.ShoppingCartProducts) ? data.ShoppingCartProducts.map((item) => <Product data={item} />) : null}
+      {Array.isArray(data?.ShoppingCartProducts)
+        ? data.ShoppingCartProducts.map((item) => <Product data={item} />)
+        : null}
     </div>
   );
 };
