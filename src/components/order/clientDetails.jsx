b11a2e0b963be4fbceb1785ca279768fd11ef611
Antonio Ruiz <contacto.antonioruiz@gmail.com>
2026-01-12 15:49:11 +0100

    chore: update utils

--- src/components/order/clientDetails.jsx
+++ src/components/order/clientDetails.jsx
@@ -2,41 +2,70 @@ import React from "react";
 import { MapLocation } from "./../mapLocation";
 import { getFullAddress } from "../../function";
 
-export const ClientDetails = ({ data }) => {
+export const ClientDetails = ({ data, size = "normal" }) => {
   const client = data && data.Customer ? data.Customer : {};
   const address = data && data.address ? data.address : [];
 
   // Verificar si hay una dirección completa para pasar al componente MapLocation
   const fullAddress = getFullAddress(address);
 
+  const NormalDetails = ({ client, address }) => {
+    return (
+      <>
+        <div className="client-details-container">
+          <div className="client-details-header">Cliente</div>
+          <div className="client-details-sub">{client.fullname}</div>
+          <div className="client-details-sub">0 pedidos</div>
+        </div>
+        <div className="client-details-container">
+          <div className="client-details-header">Información de contacto</div>
+          <div className="client-details-sub">{client.email}</div>
+          <div className="client-details-sub">{client.phone}</div>
+        </div>
+        <div className="client-details-container">
+          <div className="client-details-header">Dirección de envío</div>
+          <div className="client-details-address">
+            {fullAddress && (
+              <MapLocation address={fullAddress} height={"100px"} />
+            )}
+            <div className="client-details-address-container">
+              <div className="client-details-address-name">{address.name}</div>
+              <div className="client-details-header">{client.fullname}</div>
+              <div className="client-details-third">{fullAddress}</div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const SmallDetails = ({ client, address }) => {
+    return (
+      <>
+        <div className="client-details-container">
+          <div className="client-details-third">{client.fullname}</div>
+        </div>
+        <div className="client-details-container">
+          <div className="client-details-header">Información de contacto</div>
+          <div className="client-details-third">
+            {fullAddress}
+            <div className="client-details-address-name">({address.name})</div>
+          </div>
+          <div className="client-details-sub">{client.email}</div>
+          <div className="client-details-sub">{client.phone}</div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="client-details">
       {client.fullname === "Cliente contado" ? (
         <div className="client-details-container">Sin datos de cliente</div>
+      ) : size === "normal" ? (
+        <NormalDetails client={client} address={address} />
       ) : (
-        <>
-          <div className="client-details-container">
-            <div className="client-details-header">Cliente</div>
-            <div className="client-details-sub">{client.fullname}</div>
-            <div className="client-details-sub">0 pedidos</div>
-          </div>
-          <div className="client-details-container">
-            <div className="client-details-header">Información de contacto</div>
-            <div className="client-details-sub">{client.email}</div>
-            <div className="client-details-sub">{client.phone}</div>
-          </div>
-          <div className="client-details-container">
-            <div className="client-details-header">Dirección de envío</div>
-            <div className="client-details-address">
-              {fullAddress && <MapLocation address={fullAddress} height={"100px"} />}
-              <div className="client-details-address-container">
-                <div className="client-details-address-name">{address.name}</div>
-                <div className="client-details-header">{client.fullname}</div>
-                <div className="client-details-third">{fullAddress}</div>
-              </div>
-            </div>
-          </div>
-        </>
+        <SmallDetails client={client} address={address} />
       )}
     </div>
   );
--- src/components/order/orderInfo.jsx
+++ src/components/order/orderInfo.jsx
@@ -14,20 +14,13 @@ export const OrderInfo = ({ data }) => {
             <div className="order-value">{data?.number}</div>
           </div>
         </div>
-        <div className="order-header-time">
-          <div className="order-label">Tiempo de preparación</div>
-          <div className="order-value">00:00:00</div>
-        </div>
-      </div>
-      <div className="order-header-2">
         <div className="order-info">
           <div className="order-info-time">
-            <div className="order-label">Fecha del pedido</div>
-            <div className="order-value">{parseDate(data?.createdAt)}</div>
+            <div className="order-label text-align-right">Fecha del pedido</div>
+            <div className="order-value text-align-right">
+              {parseDate(data?.createdAt)}
+            </div>
           </div>
-          {/*<button className="order-info-button">
-          Hacer devolución
-        </button>*/}
         </div>
       </div>
     </>
